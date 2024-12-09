76e19b467e7231416c3e4220c7ed5f40ef40afcd
code043 <procoder043@gmail.com>
2024-12-09 17:58:25 -0300

    Start server

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", home)
+
+	// Start
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 func home(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "home.html")
