ade82ae29d51c7a3a1736bdfa70d979a1f8d9b61
code043 <procoder043@gmail.com>
2024-12-09 20:42:51 -0300

    Routes

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/projects", projects)
+	http.HandleFunc("/skills", home)
+	http.HandleFunc("/about", home)
+	http.HandleFunc("/contact", home)
 
 	// Start
 	err := http.ListenAndServe(":3000", nil)
@@ -17,6 +21,18 @@ func main() {
 }
 func home(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "home.html")
+}
+func projects(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "projects.html")
+}
+func skills(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "skills.html")
+}
+func about(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.html")
+}
+func contact(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "contact.html")
 }
 func renderTemplate(w http.ResponseWriter, tmpl string) {
 	t, err := template.ParseFiles("templates/" + tmpl)
